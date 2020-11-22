419d7eba7a88d8c8dddeea6d1cec473987f35578
Mike Hoskins <mrh@devopsdreams.io>
2020-11-22 17:53:39 -0500

    union types

--- 001-using-types/app.js
+++ 001-using-types/app.js
@@ -1,46 +1,14 @@
-// explicit object type...
-// not needed when all types can be infered
-// const person: {
-//     name: string
-//     age: number
-// } = {
-// const person: {
-//     name: string
-//     age: number
-//     hobbies: string[]
-//     role: [number, string] // tuple
-// } = { // type inference (preferred)
-//     name: 'Neo',
-//     age: 42,
-//     hobbies: ['Hacking', 'Kung-Fu'],
-//     role: [2, 'author']
-// }
-// for generic array type...
-// person.role.push('admin') // would work even if bad, including with tuple!
-// person.role[1] = 10 // prevented by tuple
-// person.role = [0, 'admin', 'user'] // prevented by tuple
-// const ADMIN = 0
-// const READ_ONLY = 1
-// const AUTHOR = 2
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Neo',
-    age: 42,
-    hobbies: ['Hacking', 'Kung-Fu'],
-    role: Role.ADMIN
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
