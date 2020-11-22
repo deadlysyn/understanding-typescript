ce6017747bf3a90c34ed359be02db25effa15b4c
Mike Hoskins <mrh@devopsdreams.io>
2020-11-22 17:34:51 -0500

    tuples

--- 001-using-types/app.ts
+++ 001-using-types/app.ts
@@ -1,13 +1,25 @@
 // explicit object type...
+// not needed when all types can be infered
 // const person: {
 //     name: string
 //     age: number
 // } = {
-const person = { // type inference (preferred)
+const person: {
+    name: string
+    age: number
+    hobbies: string[]
+    role: [number, string] // tuple
+} = { // type inference (preferred)
     name: 'Neo',
     age: 42,
-    hobbies: ['Hacking', 'Kung-Fu']
+    hobbies: ['Hacking', 'Kung-Fu'],
+    role: [2, 'author']
 }
+
+// for generic array type...
+// person.role.push('admin') // would work even if bad, including with tuple!
+// person.role[1] = 10 // prevented by tuple
+// person.role = [0, 'admin', 'user'] // prevented by tuple
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
