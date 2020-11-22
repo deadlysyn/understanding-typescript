07b2e16829d439408f4ea28e0879e159f3718619
Mike Hoskins <mrh@devopsdreams.io>
2020-11-22 17:43:33 -0500

    enums

--- 001-using-types/app.js
+++ 001-using-types/app.js
@@ -1,12 +1,38 @@
 // explicit object type...
+// not needed when all types can be infered
 // const person: {
 //     name: string
 //     age: number
 // } = {
+// const person: {
+//     name: string
+//     age: number
+//     hobbies: string[]
+//     role: [number, string] // tuple
+// } = { // type inference (preferred)
+//     name: 'Neo',
+//     age: 42,
+//     hobbies: ['Hacking', 'Kung-Fu'],
+//     role: [2, 'author']
+// }
+// for generic array type...
+// person.role.push('admin') // would work even if bad, including with tuple!
+// person.role[1] = 10 // prevented by tuple
+// person.role = [0, 'admin', 'user'] // prevented by tuple
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Neo',
     age: 42,
-    hobbies: ['Hacking', 'Kung-Fu']
+    hobbies: ['Hacking', 'Kung-Fu'],
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ['Sports'];
@@ -14,4 +40,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- 001-using-types/app.ts
+++ 001-using-types/app.ts
@@ -4,22 +4,43 @@
 //     name: string
 //     age: number
 // } = {
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string] // tuple
-} = { // type inference (preferred)
-    name: 'Neo',
-    age: 42,
-    hobbies: ['Hacking', 'Kung-Fu'],
-    role: [2, 'author']
-}
+// const person: {
+//     name: string
+//     age: number
+//     hobbies: string[]
+//     role: [number, string] // tuple
+// } = { // type inference (preferred)
+//     name: 'Neo',
+//     age: 42,
+//     hobbies: ['Hacking', 'Kung-Fu'],
+//     role: [2, 'author']
+// }
 
 // for generic array type...
 // person.role.push('admin') // would work even if bad, including with tuple!
 // person.role[1] = 10 // prevented by tuple
 // person.role = [0, 'admin', 'user'] // prevented by tuple
+
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+// numbering starts at 0
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+// start at specified number
+// enum Role { ADMIN = 5, READ_ONLY, AUTHOR }
+// specify all
+// enum Role { ADMIN = 5, READ_ONLY = 10, AUTHOR = 20 }
+// mixed types...
+// enum Role { ADMIN = 5, READ_ONLY = 'FOO', AUTHOR = 20 }
+
+const person = {
+    name: 'Neo',
+    age: 42,
+    hobbies: ['Hacking', 'Kung-Fu'],
+    role: Role.ADMIN
+}
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
@@ -28,4 +49,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin')
 }
